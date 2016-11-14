bc1394e0adaac193b23bcbac35bf5693eadc2dc0
mikesebald <michael.sebald@gmail.com>
2016-11-14 21:41:30 +0100

    wip

--- mongobiconnector.R
+++ mongobiconnector.R
@@ -2,17 +2,18 @@ library(RODBC)
 
 odbc.database = "biuser"
 odbc.user = "biuser"
-odbc.password = "test123Abc"
+odbc.password = "test77"
 
 bi_odbc <- odbcConnect("MongoBI", uid = odbc.user, pwd = odbc.password)
-
-foo <- sqlQuery(bi_odbc, 
-                as.is = TRUE, 
-                query = "select * from foo")
 
 odbcGetInfo(bi_odbc)
 
 bi_tables <- sqlTables(bi_odbc)
+
+record_actual <- sqlQuery(bi_odbc, query = "select * from record_actual")
+
+postal_address <- sqlQuery(bi_odbc,
+                           query = "select * from record_actual_merged_postal_address")
 
 odbcClose(bi_odbc)
 
@@ -20,8 +21,16 @@ odbcClose(bi_odbc)
 
 record_actual_cols <- sqlColumns(bi_odbc, "record_actual")
 
-record_actual <- sqlQuery(bi_odbc, as.is = TRUE, 
-                          query = "select * from record_actual")
+
+foo <- sqlQuery(bi_odbc, 
+                as.is = TRUE, 
+                query = "select * from foo")
+
+
+sqlQuery(bi_odbc, "SELECT * FROM record_actual")
+
+
+record_actual <- sqlQuery(bi_odbc, query = "select * from record_actual")
 
 record_actual_merged_party <- sqlQuery(bi_odbc, as.is = TRUE, 
                                        query = "select * from \
